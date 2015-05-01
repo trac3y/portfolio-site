489164c7804c2998268252e4334aa24d0041e0e7
Tracey Lum <drylikestalebread@gmail.com>
2015-04-30 21:37:31 -0400

    adding files

---
+++ app/scripts/main.js
@@ -0,0 +1,41 @@
+//to collapse the navbar on scroll
+$(window).scroll(function() {
+	'use strict';
+	// fade in words
+	if ($('.navbar').offset().top > 30) {
+		$('#fade').fadeIn(3000);
+	} 
+	// make nav sticky
+	if ($('.navbar').offset().top > 50) {
+		$('.navbar-fixed-top').addClass('top-nav-collapse');
+		$('.navbar').addClass('sticky');
+	} else {
+		$('.navbar-fixed-top').removeClass('top-nav-collapse');
+		$('.navbar').removeClass('sticky');
+	}
+});
+
+//to scroll to section
+$(function() {
+	'use strict';
+	$('a.page-scroll').bind('click', function(event) {
+	//window.alert('clicked a thing');
+	var $anchor = $(this);
+	$('html, body').stop().animate({
+		scrollTop: $($anchor.attr('href')).offset().top
+	}, 1500, 'easeInOutExpo');
+	event.preventDefault();
+});
+});
+
+//to animate the interest list
+$('.interests-list').hide();
+
+$('#interests').click(function() {
+	'use strict';
+//window.alert('clicked interests');
+$(this).find('ul').stop().animate({
+	width:'toggle'
+});
+});
+
